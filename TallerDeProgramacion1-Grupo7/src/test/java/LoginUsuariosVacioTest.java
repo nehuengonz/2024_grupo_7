5bbcd28836f0dcdf15268079670570cbc4bcde53
facundodelga <128828081+facundodelga@users.noreply.github.com>
2024-09-27 19:29:28 -0300

    Create LoginUsuariosVacioTest.java

---
+++ TallerDeProgramacion1-Grupo7/src/test/java/LoginUsuariosVacioTest.java
@@ -0,0 +1,103 @@
+import excepciones.PasswordErroneaException;
+import excepciones.UsuarioNoExisteException;
+import modeloNegocio.Empresa;
+import org.junit.*;
+import org.junit.runner.JUnitCore;
+
+import static org.junit.Assert.fail;
+
+public class LoginUsuariosVacioTest {
+    private Empresa empresa;
+
+    public LoginUsuariosVacioTest() {
+
+    }
+
+    public static void main(String[] args) {
+        String[] args2 = { LoginUsuariosVacioTest.class.getName() };
+        JUnitCore.main(args2);
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        Empresa.getInstance();
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
+
+
+    @Test
+    public void testLoginAdmin(){
+        try {
+            Empresa.getInstance().login("admin","admin");
+            Assert.assertTrue("Se logueo un Administrador",Empresa.getInstance().isAdmin());
+        } catch (UsuarioNoExisteException e) {
+            fail("No deberia tirar este error porque el admin ya deberia estar contemplado");
+        } catch (PasswordErroneaException e) {
+            fail("No deberia tirar este error porque la contasenia admin ya deberia estar contemplado");
+        }
+    }
+
+    @Test
+    public void testLoginAdminMalUsuario(){
+        try {
+            Empresa.getInstance().login("a","admin");
+            fail("Se logueo un Administrador");
+        } catch (UsuarioNoExisteException e) {
+            Assert.assertNull(Empresa.getInstance().getUsuarioLogeado());
+        } catch (PasswordErroneaException e) {
+            fail("No deberia tirar este error");
+        }
+    }
+
+    @Test
+    public void testLoginAdminMalContra(){
+        try {
+            Empresa.getInstance().login("admin","123");
+            fail("Se logueo un Administrador");
+        } catch (UsuarioNoExisteException e) {
+            fail("No deberia tirar este error");
+        } catch (PasswordErroneaException e) {
+            Assert.assertNull(Empresa.getInstance().getUsuarioLogeado());
+        }
+    }
+
+    @Test
+    public void testLoginUsuarioNoExiste(){
+        try {
+            Empresa.getInstance().login("a","123");
+            fail("Se logueo un Cliente");
+        } catch (UsuarioNoExisteException e) {
+            Assert.assertNull(Empresa.getInstance().getUsuarioLogeado());
+
+        } catch (PasswordErroneaException e) {
+            fail("No deberia tirar este error");
+        }
+    }
+
+    @Test
+    public void testLoginContraseñaNoExiste(){
+        try {
+            Empresa.getInstance().login("a","123");
+            fail("Se logueo un Cliente");
+        } catch (UsuarioNoExisteException e) {
+            Assert.assertNull(Empresa.getInstance().getUsuarioLogeado());
+        } catch (PasswordErroneaException e) {
+            fail("No deberia tirar este error porque directamente esta vacio el arreglo de clientes");
+        }
+    }
+
+}
