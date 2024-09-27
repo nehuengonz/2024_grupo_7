0a6331bdac0aa8ed80c5c68446efae6553b4c988
facundodelga <128828081+facundodelga@users.noreply.github.com>
2024-09-27 16:51:54 -0300

    commit con esqueletos para los test

---
+++ tests/test/EscenarioVacio.java
@@ -0,0 +1,17 @@
+package test;
+
+public class EscenarioVacio {
+    public Object sistema;
+
+    public EscenarioVacio(){
+
+    }
+
+    public void setUp(){
+        sistema = new Object();
+    }
+
+    public void tearDown(){
+        sistema = null;
+    }
+}
---
+++ tests/test/TestVacio.java
@@ -0,0 +1,43 @@
+package test;
+
+import org.junit.*;
+import org.junit.runner.JUnitCore;
+
+
+public class TestVacio {
+    private EscenarioVacio escenarioVacio = new EscenarioVacio();
+
+    public TestVacio() {
+    }
+
+    public static void main(String[] args) {
+        String[] args2 = { TestVacio.class.getName() };
+        JUnitCore.main(args2);
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        escenarioVacio.setUp();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        escenarioVacio.tearDown();
+    }
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
+
+
+    @Test
+    public void testeaAlgo(){
+
+    }
+
+}
