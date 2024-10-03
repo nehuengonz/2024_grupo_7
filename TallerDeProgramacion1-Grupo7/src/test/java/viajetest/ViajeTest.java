99bc566f7e164de7ca67c51b5bd41f8b1210a1b3
facundodelga <128828081+facundodelga@users.noreply.github.com>
2024-10-03 18:17:01 -0300

    Update ViajeTest.java

--- TallerDeProgramacion1-Grupo7/src/test/java/viajetest/ViajeTest.java
+++ TallerDeProgramacion1-Grupo7/src/test/java/viajetest/ViajeTest.java
@@ -9,10 +9,14 @@ import org.junit.runner.JUnitCore;
 import util.Constantes;
 
 public class ViajeTest {
-    Viaje viajeSinExtras_Standard;
-    Viaje viajeConExtras_Peligrosa;
-    Viaje viajeConExtras_SinAsfaltar;
-    Viaje viajeSinExtras_SinAsfaltar;
+    private Viaje viajeSinExtras_Standard;
+    private Viaje viajeConMascota_Peligrosa;
+    private Viaje viajeConBaul_SinAsfaltar;
+    private Viaje viajeSinExtras_SinAsfaltar;
+    private Viaje viajeConExtras_Standard;
+    private Viaje viajeSinExtras_Peligrosa;
+    private Viaje viajeConMascota_Standard;
+
     public static void main(String[] args) {
         String[] args2 = { ViajeTest.class.getName() };
         JUnitCore.main(args2);
@@ -22,57 +26,97 @@ public class ViajeTest {
     public void setUp(){
         Viaje.setValorBase(5.0);
 
-        Moto v = new Moto("abc123");
+        Auto v = new Auto("abc123",4,true);
         Chofer c = new ChoferPermanente("1234567", "Facundo",2000, 2 );
         Cliente cliente = new Cliente("Roberto","123", "Roberto");
-        Pedido p = new Pedido(cliente, 1, false, false, 10, Constantes.ZONA_STANDARD);
 
+        //CASO BASE
+        //Sin incrementos, zona standard. Caso base
+        Pedido p = new Pedido(cliente, 1, false, false, 10, Constantes.ZONA_STANDARD);
         this.viajeSinExtras_Standard = new Viaje(p,c,v);
 
-        Auto v1 = new Auto("abc123",4,true);
-        Pedido p1 = new Pedido(cliente, 4, true, true, 10, Constantes.ZONA_PELIGROSA);
+        //ZONAS
+        //sin incrementos zona peligrosa
+        Pedido p1 = new Pedido(cliente, 4, false, false, 10, Constantes.ZONA_PELIGROSA);
+        this.viajeSinExtras_Peligrosa = new Viaje(p1,c,v);
 
-        this.viajeConExtras_Peligrosa = new Viaje(p1,c,v1);
+        //Sin incrementos zona sin asfaltar
+        Pedido p2 = new Pedido(cliente, 1, false, false, 10, Constantes.ZONA_SIN_ASFALTAR);
+        this.viajeSinExtras_SinAsfaltar = new Viaje(p2,c,v);
 
-        Pedido p2 = new Pedido(cliente, 4, true, true, 10, Constantes.ZONA_SIN_ASFALTAR);
+        //CON MASCOTA
+        Pedido p3 = new Pedido(cliente, 1, true, false, 10, Constantes.ZONA_STANDARD);
+        this.viajeConMascota_Standard = new Viaje(p3,c,v);
 
-        this.viajeConExtras_SinAsfaltar = new Viaje(p2,c,v1);
+        //COMBINACIONES
+        //Zona peligrosa con mascota
+        Pedido p4 = new Pedido(cliente, 4, true, false, 10, Constantes.ZONA_PELIGROSA);
+        this.viajeConMascota_Peligrosa = new Viaje(p4,c,v);
 
-        Pedido p3 = new Pedido(cliente, 1, false, false, 10, Constantes.ZONA_SIN_ASFALTAR);
+        //Zona sin asfaltar con baul
+        Pedido p5 = new Pedido(cliente, 4, false, true, 10, Constantes.ZONA_SIN_ASFALTAR);
+        this.viajeConBaul_SinAsfaltar = new Viaje(p5,c,v);
 
-        this.viajeSinExtras_SinAsfaltar = new Viaje(p3,c,v);
     }
+
+//    Viaje sin incrementos standard 10.5
+//    Viaje sin incrementos peligrosa 17.0
+//    Viaje sin incrementos sin asfaltar 13.5
+//    Viaje con mascota standard 21.0
+//    Viaje con mascota peligrosa 29.0
+//    Viaje con baul sin asfaltar 21.0
 
     @After
     public void tearDown(){
 
         viajeSinExtras_Standard = null;
-        viajeConExtras_Peligrosa = null;
+        viajeConMascota_Peligrosa = null;
     }
 
+    //CASO BASE
+    //Sin incrementos, zona standard. Caso base
     @Test
-    public void getValorSinExtras_StandardTest(){
+    public void getValorSinIncrementos_StandardTest(){
 
         Assert.assertEquals("No se calcula bien el valor del viaje sin extras y estandard",10.5, viajeSinExtras_Standard.getValor(),0.0000001);
     }
 
+    //Sin incrementos, zona peligrosa.
     @Test
-    public void getValorConExtras_PeligrosaTest(){
+    public void getValorSinIncrementos_PeligrosaTest(){
 
-        Assert.assertEquals("No se calcula bien el valor del viaje con extras y zona peligrosa",33.5, viajeConExtras_Peligrosa.getValor(),0.0000001);
+        Assert.assertEquals("No se calcula bien el valor del viaje sin incrementos zona peligrosa",17.0, viajeSinExtras_Peligrosa.getValor(),0.0000001);
     }
 
-    @Test
-    public void getValorConExtras_SinAsfaltarTest(){
-
-        Assert.assertEquals("No se calcula bien el valor del viaje con extras Sin Asfaltar",33.0,viajeConExtras_SinAsfaltar.getValor(),0.0000001);
-    }
-
+    //Sin incrementos zona sin asfaltar
     @Test
     public void getValorSinExtras_SinAsfaltarTest(){
 
-        Assert.assertEquals("No se calcula bien el valor del viaje sin extras Sin Asfaltar",13.5,viajeSinExtras_SinAsfaltar.getValor(),0.0000001);
+        Assert.assertEquals("No se calcula bien el valor del viaje sin incrementos Sin Asfaltar",13.5,viajeSinExtras_SinAsfaltar.getValor(),0.0000001);
     }
+
+    //CON MASCOTA
+    @Test
+    public void getValorConMascota_StandardTest(){
+
+        Assert.assertEquals("No se calcula bien el valor del viaje con mascota estandar",21.0, viajeConMascota_Standard.getValor(),0.0000001);
+    }
+
+    //Zona sin asfaltar con mascota zona peligrosa
+    @Test
+    public void getValorConMascota_PeligrosaTest(){
+
+        Assert.assertEquals("No se calcula bien el valor del viaje con mascota zona peligrosa",29.0, viajeConMascota_Peligrosa.getValor(),0.0000001);
+    }
+
+    //Zona sin asfaltar con baul
+    @Test
+    public void getValorConBaul_SinAsfaltarTest(){
+
+        Assert.assertEquals("No se calcula bien el valor del viaje con baul sin asfaltar",21.0, viajeConBaul_SinAsfaltar.getValor(),0.0000001);
+    }
+
+
 
     @Test
     public void finalizarViajeTest(){
