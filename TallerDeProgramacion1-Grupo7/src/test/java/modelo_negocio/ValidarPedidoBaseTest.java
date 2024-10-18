ea02980978b45603e742f9f757b756342711e397
facundodelga <128828081+facundodelga@users.noreply.github.com>
2024-10-18 11:32:20 -0300

    validar pedido test

---
+++ TallerDeProgramacion1-Grupo7/src/test/java/modelo_negocio/ValidarPedidoBaseTest.java
@@ -0,0 +1,29 @@
+package modelo_negocio;
+
+import modeloDatos.Auto;
+import modeloDatos.Cliente;
+import modeloDatos.Pedido;
+import modeloDatos.Vehiculo;
+import modeloNegocio.Empresa;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import util.Constantes;
+
+public class ValidarPedidoBaseTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void validarPedidoNoExisteVehiculoTest() {
+
+    }
+}
---
+++ TallerDeProgramacion1-Grupo7/src/test/java/modelo_negocio/ValidarPedidoEscenario2Test.java
@@ -0,0 +1,38 @@
+package modelo_negocio;
+
+import modeloDatos.Cliente;
+import modeloDatos.Pedido;
+import modeloNegocio.Empresa;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import test.EscenarioVacio;
+
+import java.util.HashMap;
+
+public class ValidarPedidoEscenario2Test {
+    private Escenario2 escenario2 = new Escenario2();
+
+    @Before
+    public void setUp() throws Exception {
+        escenario2.setup();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        escenario2.tearDown();
+    }
+
+    @Test
+    public void validarPedidoExisteVehiculoTest() {
+        HashMap<Cliente, Pedido> pedidos = Empresa.getInstance().getPedidos();
+        Cliente cliente = Empresa.getInstance().getClientes().get("facundo");
+        Pedido pedidoValidar = pedidos.get(cliente);
+
+        Assert.assertTrue("Se supone que existe un vehiculo que lo puede satisfacer",Empresa.getInstance().validarPedido(pedidoValidar));
+    }
+
+
+
+}
