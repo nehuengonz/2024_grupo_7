8709be475b09b008912dc79c865ee621a857b59f
facundodelga <128828081+facundodelga@users.noreply.github.com>
2024-10-19 19:06:36 -0300

    Update PagarYFinalizarViajeEscenario3Test.java

--- TallerDeProgramacion1-Grupo7/src/test/java/modelo_negocio/PagarYFinalizarViajeEscenario3Test.java
+++ TallerDeProgramacion1-Grupo7/src/test/java/modelo_negocio/PagarYFinalizarViajeEscenario3Test.java
@@ -8,6 +8,8 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import util.Constantes;
+import util.Mensajes;
 
 import java.util.HashMap;
 
@@ -33,6 +35,7 @@ public class PagarYFinalizarViajeEscenario3Test {
     @Test
     public void pagarYFinalizarViajeTest() {
         try {
+
             Cliente clienteLogueado = Empresa.getInstance().getClientes().get(Empresa.getInstance().getUsuarioLogeado().getNombreUsuario());
             Viaje v = Empresa.getInstance().getViajeDeCliente(clienteLogueado);
             Empresa.getInstance().pagarYFinalizarViaje(5);
@@ -42,6 +45,22 @@ public class PagarYFinalizarViajeEscenario3Test {
 
         } catch (ClienteSinViajePendienteException e) {
             fail("El cliente tiene un viaje pendiente de terminar");
+        }
+    }
+
+    @Test
+    public void pagarYFinalizarViajeClienteSinViajeTest() {
+        try {
+            Cliente clienteLogueado = Empresa.getInstance().getClientes().get(Empresa.getInstance().getUsuarioLogeado().getNombreUsuario());
+            Empresa.getInstance().pagarYFinalizarViaje(5);
+            Viaje v = Empresa.getInstance().getViajeDeCliente(clienteLogueado);
+            Empresa.getInstance().pagarYFinalizarViaje(5);
+
+
+            Assert.assertEquals("No se guardo bien la calificacion del viaje",v.getCalificacion(),5);
+
+        } catch (ClienteSinViajePendienteException e) {
+            Assert.assertEquals("El mensaje esta mal",e.getMessage(), Mensajes.CLIENTE_SIN_VIAJE_PENDIENTE.getValor());
         }
     }
 
