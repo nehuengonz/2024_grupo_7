6ce40f4cd570cbe375d4361f84ba39c0af2fa8e3
SofiaIPalladino <sofia.palladino.2001@gmail.com>
2024-10-23 12:55:17 -0300

    creo Escenario 6

--- TallerDeProgramacion1-Grupo7/src/test/java/test/TestVehiculosOrdenadosPorPedidoEscenario2.java
+++ TallerDeProgramacion1-Grupo7/src/test/java/test/TestVehiculosOrdenadosPorPedidoEscenario2.java
@@ -41,5 +41,20 @@ public class TestVehiculosOrdenadosPorPedidoEscenario2 {
         assertEquals("El segundo vehículo con mayor puntaje debe ser combi111.", "combi111", vehiculosOrdenados.get(1).getPatente());
     }
 
-    
+    @Test
+    public void testVehiculosOrdenadosPorPedidoSinVehiculoHabilitado() {
+        Cliente cliente = Empresa.getInstance().getClientes().get("facundo");
+        Pedido pedido = Empresa.getInstance().getPedidos().get(cliente);
+
+        ArrayList<Vehiculo> vehiculosOrdenados = Empresa.getInstance().vehiculosOrdenadosPorPedido(pedido);
+
+        assertNotNull("La lista de vehículos ordenados no debería ser nula.", vehiculosOrdenados);
+
+        assertEquals("Se esperaban 2 vehículos ordenados.", 2, vehiculosOrdenados.size());
+
+        assertEquals("El vehículo con mayor puntaje debe ser ABC123.", "abc123", vehiculosOrdenados.get(0).getPatente());
+        assertEquals("El segundo vehículo con mayor puntaje debe ser combi111.", "combi111", vehiculosOrdenados.get(1).getPatente());
+    }
+
+
 }
