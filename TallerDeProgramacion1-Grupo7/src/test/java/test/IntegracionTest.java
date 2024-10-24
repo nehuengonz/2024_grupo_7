7b129ff5dc7ed3616ed92a676a07ea2dcb54e70b
facundodelga <128828081+facundodelga@users.noreply.github.com>
2024-10-24 20:15:31 -0300

    la altas estan hechas

--- TallerDeProgramacion1-Grupo7/src/test/java/test/IntegracionTest.java
+++ TallerDeProgramacion1-Grupo7/src/test/java/test/IntegracionTest.java
@@ -1,9 +1,7 @@
 package test;
 
 import controlador.Controlador;
-import modeloDatos.Chofer;
-import modeloDatos.ChoferPermanente;
-import modeloDatos.ChoferTemporario;
+import modeloDatos.*;
 import modeloNegocio.Empresa;
 import org.junit.After;
 import org.junit.Assert;
@@ -43,6 +41,7 @@ public class IntegracionTest {
 
         controlador = new Controlador();
         controlador.setPersistencia(persistencia);
+        controlador.setVista(vista);
 
         when(vista.getOptionPane()).thenReturn(ventanaErrores);
 
@@ -89,7 +88,6 @@ public class IntegracionTest {
         when(vista.getNombreChofer()).thenReturn("Alberto");
         when(vista.getDNIChofer()).thenReturn("123456789");
 
-        controlador.setVista(vista);
         controlador.actionPerformed(new ActionEvent(this,1,Constantes.LOGIN)); //login
 
         Assert.assertNotNull("Usuario logueado incorrectamente", Empresa.getInstance().getUsuarioLogeado());
@@ -129,7 +127,6 @@ public class IntegracionTest {
         when(vista.getAnioChofer()).thenReturn(2020);
         when(vista.getHijosChofer()).thenReturn(1);
 
-        controlador.setVista(vista);
         controlador.actionPerformed(new ActionEvent(this,1,Constantes.LOGIN)); //login
 
         Assert.assertNotNull("Usuario logueado incorrectamente", Empresa.getInstance().getUsuarioLogeado());
@@ -173,7 +170,6 @@ public class IntegracionTest {
         when(vista.getAnioChofer()).thenReturn(2020);
         when(vista.getHijosChofer()).thenReturn(1);
 
-        controlador.setVista(vista);
         controlador.actionPerformed(new ActionEvent(this,1,Constantes.LOGIN)); //login
 
         Assert.assertNotNull("Usuario logueado incorrectamente", Empresa.getInstance().getUsuarioLogeado());
@@ -195,6 +191,195 @@ public class IntegracionTest {
         Assert.assertEquals("El mensaje de error no es el correcto", ventanaErrores.getMensajeError(), Mensajes.CHOFER_YA_REGISTRADO.getValor());
     }
 
+    @Test
+    public void loginAdmin_y_nuevoVehiculoMoto() throws IOException, ClassNotFoundException {
+        when(vista.getPassword()).thenReturn("admin");
+        when(vista.getUsserName()).thenReturn("admin");
+
+        when(vista.getTipoVehiculo()).thenReturn(Constantes.MOTO);
+        when(vista.getPatente()).thenReturn("abc123");
+
+        controlador.actionPerformed(new ActionEvent(this,1,Constantes.LOGIN)); //login
+
+        Assert.assertNotNull("Usuario logueado incorrectamente", Empresa.getInstance().getUsuarioLogeado());
+        Assert.assertEquals("Usuario logueado incorrectamente", Empresa.getInstance().getUsuarioLogeado().getNombreUsuario(), "admin");
+        Assert.assertEquals("Usuario logueado incorrectamente", Empresa.getInstance().getUsuarioLogeado().getPass(), "admin");
+
+        controlador.actionPerformed(new ActionEvent(this,2,Constantes.NUEVO_VEHICULO)); //agregar chofer
+
+        Moto moto = (Moto) Empresa.getInstance().getVehiculos().get("abc123");
+
+        Assert.assertNotNull("Moto no fue agregada correctamente", moto);
+        Assert.assertEquals("Moto no fue agregado correctamente", moto.getPatente(),"abc123");
+        Assert.assertEquals("Moto no fue agregado correctamente", moto.getCantidadPlazas(),1);
+        Assert.assertFalse("Moto no fue agregado correctamente", moto.isMascota());
+
+        controlador.actionPerformed(new ActionEvent(this,3,Constantes.CERRAR_SESION_ADMIN)); //logout
+
+        Assert.assertNull("No se deslogueo al usuario", Empresa.getInstance().getUsuarioLogeado());
+
+        persistencia.abrirInput("empresa.bin");
+        EmpresaDTO empresaLeida = (EmpresaDTO) persistencia.leer();
+        persistencia.cerrarInput();
+
+        Moto motoLeido = (Moto) empresaLeida.getVehiculos().get("abc123");
+
+        Assert.assertEquals("Moto no fue persistido correctamente", motoLeido.getPatente(),"abc123");
+    }
+
+    @Test
+    public void loginAdmin_y_nuevoVehiculoAuto() throws IOException, ClassNotFoundException {
+        when(vista.getPassword()).thenReturn("admin");
+        when(vista.getUsserName()).thenReturn("admin");
+
+        when(vista.getTipoVehiculo()).thenReturn(Constantes.AUTO);
+        when(vista.getPatente()).thenReturn("abc123");
+        when(vista.getPlazas()).thenReturn(4);
+        when(vista.isVehiculoAptoMascota()).thenReturn(true);
+
+        controlador.actionPerformed(new ActionEvent(this,1,Constantes.LOGIN)); //login
+
+        Assert.assertNotNull("Usuario logueado incorrectamente", Empresa.getInstance().getUsuarioLogeado());
+        Assert.assertEquals("Usuario logueado incorrectamente", Empresa.getInstance().getUsuarioLogeado().getNombreUsuario(), "admin");
+        Assert.assertEquals("Usuario logueado incorrectamente", Empresa.getInstance().getUsuarioLogeado().getPass(), "admin");
+
+        controlador.actionPerformed(new ActionEvent(this,2,Constantes.NUEVO_VEHICULO)); //agregar chofer
+
+        Auto auto = (Auto) Empresa.getInstance().getVehiculos().get("abc123");
+
+        Assert.assertNotNull("auto no fue agregada correctamente", auto);
+        Assert.assertEquals("auto no fue agregado correctamente", auto.getPatente(),"abc123");
+        Assert.assertEquals("auto no fue agregado correctamente", auto.getCantidadPlazas(),4);
+        Assert.assertTrue("auto no fue agregado correctamente", auto.isMascota());
+
+        controlador.actionPerformed(new ActionEvent(this,3,Constantes.CERRAR_SESION_ADMIN)); //logout
+
+        Assert.assertNull("No se deslogueo al usuario", Empresa.getInstance().getUsuarioLogeado());
+
+        persistencia.abrirInput("empresa.bin");
+        EmpresaDTO empresaLeida = (EmpresaDTO) persistencia.leer();
+        persistencia.cerrarInput();
+
+        Auto autoLeido = (Auto) empresaLeida.getVehiculos().get("abc123");
+
+        Assert.assertEquals("Auto no fue persistido correctamente", autoLeido.getPatente(),"abc123");
+        Assert.assertEquals("auto no fue agregado correctamente", autoLeido.getCantidadPlazas(),4);
+        Assert.assertTrue("auto no fue agregado correctamente", autoLeido.isMascota());
+    }
+
+    @Test
+    public void loginAdmin_y_nuevoVehiculoCombi() throws IOException, ClassNotFoundException {
+        when(vista.getPassword()).thenReturn("admin");
+        when(vista.getUsserName()).thenReturn("admin");
+
+        when(vista.getTipoVehiculo()).thenReturn(Constantes.COMBI);
+        when(vista.getPatente()).thenReturn("abc123");
+        when(vista.getPlazas()).thenReturn(10);
+        when(vista.isVehiculoAptoMascota()).thenReturn(false);
+
+        controlador.actionPerformed(new ActionEvent(this,1,Constantes.LOGIN)); //login
+
+        Assert.assertNotNull("Usuario logueado incorrectamente", Empresa.getInstance().getUsuarioLogeado());
+        Assert.assertEquals("Usuario logueado incorrectamente", Empresa.getInstance().getUsuarioLogeado().getNombreUsuario(), "admin");
+        Assert.assertEquals("Usuario logueado incorrectamente", Empresa.getInstance().getUsuarioLogeado().getPass(), "admin");
+
+        controlador.actionPerformed(new ActionEvent(this,2,Constantes.NUEVO_VEHICULO)); //agregar chofer
+
+        Combi combi = (Combi) Empresa.getInstance().getVehiculos().get("abc123");
+
+        Assert.assertNotNull("combi no fue agregada correctamente", combi);
+        Assert.assertEquals("combi no fue agregado correctamente", combi.getPatente(),"abc123");
+        Assert.assertEquals("combi no fue agregado correctamente", combi.getCantidadPlazas(),10);
+        Assert.assertFalse("combi no fue agregado correctamente", combi.isMascota());
+
+        controlador.actionPerformed(new ActionEvent(this,3,Constantes.CERRAR_SESION_ADMIN)); //logout
+
+        Assert.assertNull("No se deslogueo al usuario", Empresa.getInstance().getUsuarioLogeado());
+
+        persistencia.abrirInput("empresa.bin");
+        EmpresaDTO empresaLeida = (EmpresaDTO) persistencia.leer();
+        persistencia.cerrarInput();
+
+        Combi combiLeido = (Combi) empresaLeida.getVehiculos().get("abc123");
+
+        Assert.assertEquals("combi no fue persistido correctamente", combiLeido.getPatente(),"abc123");
+        Assert.assertEquals("combi no fue agregado correctamente", combiLeido.getCantidadPlazas(),10);
+        Assert.assertFalse("combi no fue agregado correctamente", combiLeido.isMascota());
+    }
+
+    @Test
+    public void loginAdmin_y_VehiculoRepetido()  {
+        when(vista.getPassword()).thenReturn("admin");
+        when(vista.getUsserName()).thenReturn("admin");
+
+        when(vista.getTipoVehiculo()).thenReturn(Constantes.AUTO);
+        when(vista.getPatente()).thenReturn("abc123");
+        when(vista.getPlazas()).thenReturn(4);
+        when(vista.isVehiculoAptoMascota()).thenReturn(true);
+
+        controlador.actionPerformed(new ActionEvent(this,1,Constantes.LOGIN)); //login
+
+        Assert.assertNotNull("Usuario logueado incorrectamente", Empresa.getInstance().getUsuarioLogeado());
+        Assert.assertEquals("Usuario logueado incorrectamente", Empresa.getInstance().getUsuarioLogeado().getNombreUsuario(), "admin");
+        Assert.assertEquals("Usuario logueado incorrectamente", Empresa.getInstance().getUsuarioLogeado().getPass(), "admin");
+
+        controlador.actionPerformed(new ActionEvent(this,2,Constantes.NUEVO_VEHICULO)); //agregar chofer
+
+        controlador.actionPerformed(new ActionEvent(this,3,Constantes.NUEVO_VEHICULO)); //agregar chofer
+
+        Assert.assertEquals("El mensaje de error no es el correcto", ventanaErrores.getMensajeError(), Mensajes.VEHICULO_YA_REGISTRADO.getValor());
+    }
+
+    @Test
+    public void registroDeCliente() throws IOException, ClassNotFoundException {
+        when(vista.getRegUsserName()).thenReturn("facundo");
+        when(vista.getRegNombreReal()).thenReturn("Facundo Delgado");
+        when(vista.getRegPassword()).thenReturn("123");
+        when(vista.getRegConfirmPassword()).thenReturn("123");
+
+        controlador.actionPerformed(new ActionEvent(this,1,Constantes.REG_BUTTON_REGISTRAR)); //apreta boton de registrar
+
+        Cliente cliente2 = Empresa.getInstance().getClientes().get("facundo");
+        Assert.assertNotNull("Usuario registrado incorrectamente", cliente2);
+        Assert.assertEquals("Usuario registrado incorrectamente", Empresa.getInstance().getClientes().get("facundo").getNombreUsuario(),"facundo");
+        Assert.assertEquals("Usuario registrado incorrectamente", Empresa.getInstance().getClientes().get("facundo").getNombreReal(),"Facundo Delgado");
+        Assert.assertEquals("Usuario registrado incorrectamente", Empresa.getInstance().getClientes().get("facundo").getPass(),"123");
+
+        persistencia.abrirInput("empresa.bin");
+        EmpresaDTO empresaLeida = (EmpresaDTO) persistencia.leer();
+        persistencia.cerrarInput();
+
+        Cliente cliente = empresaLeida.getClientes().get("facundo");
+        Assert.assertNotNull("Usuario persistido incorrectamente", cliente);
+        Assert.assertEquals("El usuario no fue persistido correctamente", cliente.getNombreUsuario(),"facundo");
+        Assert.assertEquals("El usuario no fue persistido correctamente", cliente.getNombreReal(),"Facundo Delgado");
+        Assert.assertEquals("El usuario no fue persistido correctamente", cliente.getPass(),"123");
+    }
+
+    @Test
+    public void registroDeClienteMalConfirmContraseña()  {
+        when(vista.getRegUsserName()).thenReturn("facundo");
+        when(vista.getRegNombreReal()).thenReturn("Facundo Delgado");
+        when(vista.getRegPassword()).thenReturn("123");
+        when(vista.getRegConfirmPassword()).thenReturn("1234");
+
+        controlador.actionPerformed(new ActionEvent(this,1,Constantes.REG_BUTTON_REGISTRAR)); //apreta boton de registrar
+        Assert.assertEquals("El mensaje de error no es el correcto", ventanaErrores.getMensajeError(), Mensajes.PASS_NO_COINCIDE.getValor());
+    }
+
+    @Test
+    public void registroDeClienteRepetido()  {
+        when(vista.getRegUsserName()).thenReturn("facundo");
+        when(vista.getRegNombreReal()).thenReturn("Facundo Delgado");
+        when(vista.getRegPassword()).thenReturn("123");
+        when(vista.getRegConfirmPassword()).thenReturn("123");
+
+        controlador.actionPerformed(new ActionEvent(this,1,Constantes.REG_BUTTON_REGISTRAR)); //apreta boton de registrar
+        controlador.actionPerformed(new ActionEvent(this,2,Constantes.REG_BUTTON_REGISTRAR)); //apreta boton de registrar
+        Assert.assertEquals("El mensaje de error no es el correcto", ventanaErrores.getMensajeError(), Mensajes.USUARIO_REPETIDO.getValor());
+    }
+
     
+
 
 }
